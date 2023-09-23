baf2b998f334ab3579b1ce72c4790a0f7acf668d
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-23 17:55:17 +0200

    add basic structure to run.py file and welcome page with input name

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#All imports for the app
+import welcome_text
+import os
+clear = lambda: os.system('clear') 
+
+answer_a = ["a", "A"]
+answer_b = ["b", "B"]
+
+
+welcome_text.welcome_logo()
+welcome_text.introduction()
+name = input('ENTER YOUR NINJA NAME >> ')
+
+clear()
+
+welcome_text.start_message(name)
+
---
+++ welcome_text.py
@@ -0,0 +1,34 @@
+def welcome_logo(): 
+    print("""
+  ______   ______        _______..___  ___.  __    ______ 
+ /      | /  __  \      /       ||   \/   | |  |  /      |
+|  ,----'|  |  |  |    |   (----`|  \  /  | |  | |  ,----'
+|  |     |  |  |  |     \   \    |  |\/|  | |  | |  |     
+|  `----.|  `--'  | .----)   |   |  |  |  | |  | |  `----.
+ \______| \______/  |_______/    |__|  |__| |__|  \______|
+      .__   __.  __  .__   __.        __       ___       __     
+      |  \ |  | |  | |  \ |  |       |  |     /   \     |  |    
+      |   \|  | |  | |   \|  |       |  |    /  ^  \    |  |    
+      |  . `  | |  | |  . `  | .--.  |  |   /  /_\  \   |  |    
+      |  |\   | |  | |  |\   | |  `--'  |  /  _____  \  |__|    
+      |__| \__| |__| |__| \__|  \______/  /__/     \__\ (__)    """)
+
+def introduction():
+    print("""
+"Starshadows: The Cosmic Ninja Chronicles"
+
+In a galaxy far beyond our own, where stars burn brighter than the sharpest blades and the cosmic winds whisper secrets of ancient arts, the Galactic Ninja Academy stands as a bastion of excellence in the art of space ninjutsu.
+You, a promising young ninja apprentice, have embarked on a journey through the cosmos to become a legendary cosmic ninja. But beware, for darkness lurks among the stars, and the shadows cast by celestial bodies hide secrets that can shape the destiny of entire galaxies.
+As you step into this cosmic adventure, your path will be marked by choices—choices that will define your legacy among the stars. Will you emerge as a hero, a master of the cosmic arts, or will you succumb to the temptations of power?
+Before we embark on this interstellar odyssey, what is your name, young ninja?
+
+""")
+
+def start_message(name):
+    print(f"""
+Welcome, {name}...
+
+The cosmic currents have whispered your name among the constellations, and the stars themselves have awaited your arrival with bated breath. You, {name}, are the starlight's chosen one, destined to carve your legend across the galaxy as a cosmic ninja.
+With shurikens of stardust and the cosmos as your canvas, you shall paint your fate among the celestial tapestry. The galaxies are your playground, and the very universe your canvas. You will harness the power of laser ninja-weapons, master the art of stealth, and unravel mysteries as vast and ancient as the cosmos itself.
+The stars align in your favor, {name}, as you embark on this epic adventure through the cosmos. Your choices will echo through the void and ripple through the fabric of space and time. Your destiny awaits among the starshadows.
+Prepare to journey beyond the stars, {name}, and may your cosmic ninja spirit burn brighter than a supernova in the night sky. The galaxy is yours to explore, to protect, and to conquer!""")
