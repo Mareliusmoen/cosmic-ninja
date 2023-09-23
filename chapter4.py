83f2e9d8ce1f75e9b8eee7e85a20e8393f1a5ff8
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-23 14:36:52 +0200

    add chapter 1, 2, 3 and partly 4 as individual py files

---
+++ chapter4.py
@@ -0,0 +1,88 @@
+chapter4aaa = print("""
+Chapter 4: Quest for the Starshard
+With Hiro as your trusted ally, you both embark on a quest to find the mysterious cosmic artifact known as the "Starshard." The information you uncovered in the ancient tome leads you to believe that it is hidden on a distant, uncharted planet at the edge of the galaxy.
+
+The journey is perilous, filled with cosmic hazards and treacherous landscapes. Along the way, you encounter strange creatures and overcome formidable challenges, relying on your ninja skills and Hiro's expertise to navigate the unknown.
+
+Finally, you arrive on the planet where the Starshard is said to be located. As you explore its ancient ruins, you stumble upon a chamber filled with intricate mechanisms and protective wards guarding the artifact.""")
+
+chapter4aab = print("""
+Chapter 4: The Council's Decision
+You and Hiro, as allies, swiftly report the existence of the Starshard and the data chips to Master Shinobi and the academy authorities. Your concerns about the potential dangers of this powerful cosmic artifact echo throughout the council chamber as you present your findings.
+
+The council members, including Master Shinobi, listen intently. They deliberate over the implications of the Starshard and its role in the cosmic ninja world. Their discussions are filled with tension, as they weigh the potential benefits and risks of possessing such an extraordinary artifact.""")
+
+chapter4aba = print("""
+Chapter 4: Quest for the Starshard
+With Hiro as your trusted ally, you both embark on a perilous quest to locate the cosmic artifact known as the "Starshard." The information you gathered from the ancient tome hints at its presence on a distant, uncharted planet at the fringes of the galaxy.
+
+The journey is fraught with cosmic dangers, from treacherous asteroid fields to gravitational anomalies. Your cosmic ninja skills and Hiro's expertise prove invaluable as you navigate these challenges together. Along the way, you encounter bizarre alien species and unlock ancient secrets that only deepen your resolve to find the Starshard.
+
+Finally, you reach the desolate planet where the Starshard is rumored to be hidden. As you explore the planet's hauntingly beautiful but perilous terrain, you come across an ancient temple that seems to be the artifact's resting place.""")
+
+chapter4abb = print("""
+Chapter 4: Council of the Stars
+Your decision to report the existence of the Starshard and the information in the ancient tome to Master Shinobi and the academy authorities sets a series of events in motion. The academy takes your concerns seriously, recognizing the gravity of the situation.
+
+A council of cosmic ninja elders and mentors convenes to deliberate over the Starshard's potential significance and the risks it may pose to the galaxy. They invite you to present your findings and insights, along with Hiro, who supports your decision.
+
+Inside the council chamber, the air is charged with anticipation. The council members, including Master Shinobi, listen carefully as you share your knowledge and concerns regarding the Starshard.""")
+
+chapter4baa = print("""
+""")
+
+chapter4bab = print("""
+""")
+
+chapter4bba = print("""
+""")
+
+chapter4bbb = print("""
+""")
+
+
+chapter4aaa_option_a = print("""
+OPTION A
+You and Hiro decide to attempt to disable the protective wards and claim the Starshard for yourselves, believing that you are the best cosmic ninjas to ensure its safekeeping.""")
+chapter4aaa_option_b = print("""
+OPTION B
+Feeling the immense power and responsibility of the Starshard, you choose to report your discovery to Master Shinobi and the academy authorities, seeking their guidance on how to handle this extraordinary cosmic artifact.""")
+
+chapter4aab_option_a = print("""
+OPTION A
+You choose to support the council's decision to secure and study the Starshard under the academy's guidance, believing that its power could be harnessed for the greater good.""")
+chapter4aab_option_b = print("""
+OPTION B
+You advocate for the complete containment and sealing of the Starshard, arguing that its potential for destruction far outweighs any benefits.""")
+
+chapter4aba_option_a = print("""
+OPTION A
+ You and Hiro decide to enter the temple and face its guardians, determined to claim the Starshard and protect it from potential threats.""")
+chapter4aba_option_b = print("""
+OPTION B
+Overwhelmed by the responsibility and potential consequences of possessing the Starshard, you choose to delay your entry into the temple and consider your next steps carefully.""")
+
+chapter4abb_option_a = print("""
+""")
+chapter4abb_option_b = print("""
+""")
+
+chapter4baa_option_a = print("""
+""")
+chapter4baa_option_b = print("""
+""")
+
+chapter4bab_option_a = print("""
+""")
+chapter4bab_option_b = print("""
+""")
+
+chapter4bba_option_a = print("""
+""")
+chapter4bba_option_b = print("""
+""")
+
+chapter4bbb_option_a = print("""
+""")
+chapter4bbb_option_b = print("""
+""")
