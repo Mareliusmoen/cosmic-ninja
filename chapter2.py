9c01ba33f35d95d3b51f56846e35e45be2ccb242
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-24 10:26:29 +0200

    edit  the content in chapter 1 and 2 files to define function correctly, and add thhe first loop and if else statement

--- chapter2.py
+++ chapter2.py
@@ -1,31 +1,55 @@
-chapter2a = print("""
+def chapter2a():
+    print("""
     Chapter 2: The Mysterious Rival
-    You decide to approach Master Shinobi with determination, introducing yourself and expressing your eagerness to embark on your cosmic ninja journey. Master Shinobi, with a wise smile, listens to your aspirations and welcomes you with a nod of approval.
+You decide to approach Master Shinobi with determination, introducing yourself and expressing your 
+eagerness to embark on your cosmic ninja journey. Master Shinobi, with a wise smile, 
+listens to your aspirations and welcomes you with a nod of approval.
+As you converse, Master Shinobi mentions a fellow student, Hiro, who has been a rising star in 
+the academy. He suggests that Hiro might make a suitable rival to push you to greater heights. 
+Master Shinobi arranges for you to meet Hiro in the academy's courtyard, where you'll have an 
+opportunity to challenge each other to a friendly duel.
+When you arrive at the courtyard, Hiro is already there, his laser ninja-weapon at the ready. 
+The duel begins, and Hiro proves to be a formidable opponent. 
+This encounter becomes a defining moment in your journey, setting the stage for a rivalry that 
+will shape your path as a cosmic ninja.""")
 
-As you converse, Master Shinobi mentions a fellow student, Hiro, who has been a rising star in the academy. He suggests that Hiro might make a suitable rival to push you to greater heights. Master Shinobi arranges for you to meet Hiro in the academy's courtyard, where you'll have an opportunity to challenge each other to a friendly duel.
-
-When you arrive at the courtyard, Hiro is already there, his laser ninja-weapon at the ready. The duel begins, and Hiro proves to be a formidable opponent. This encounter becomes a defining moment in your journey, setting the stage for a rivalry that will shape your path as a cosmic ninja.""")
-
-chapter2b = print("""
+def chapter2b():
+    print("""
     Chapter 2: The Forbidden Scrolls
-    You decide to blend into the crowd and observe the various training sessions, contemplating how you'll navigate this new world. While wandering through the academy's extensive archives, you stumble upon a section marked "Forbidden Scrolls." The scrolls are encased in a holographic shield, clearly meant to keep curious eyes away.
+You decide to blend into the crowd and observe the various training sessions, contemplating how 
+you'll navigate this new world. While wandering through the academy's extensive archives, 
+you stumble upon a section marked "Forbidden Scrolls." The scrolls are encased in a holographic 
+shield, clearly meant to keep curious eyes away.
+As you ponder the significance of these scrolls, an instructor approaches. She warns you that 
+those scrolls contain ancient and powerful cosmic ninja techniques, and they are strictly 
+off-limits to all students. The instructor tells you that such knowledge can be dangerous in the 
+wrong hands and asks if you've seen anyone tampering with them.""")
 
-As you ponder the significance of these scrolls, an instructor approaches. She warns you that those scrolls contain ancient and powerful cosmic ninja techniques, and they are strictly off-limits to all students. The instructor tells you that such knowledge can be dangerous in the wrong hands and asks if you've seen anyone tampering with them.""")
-
-chapter2a_option_a = print("""
+def chapter2a_option_a():
+    print("""
     OPTION A
-    You decide to embrace the rivalry with Hiro. 
-    You challenge Hiro to a series of sparring matches and seek to learn from each encounter. 
-    This choice will lead to further interactions with Hiro, where you'll uncover more about his character and skills, and perhaps even lead to a lethal rivalry""")
+You decide to embrace the rivalry with Hiro. 
+You challenge Hiro to a series of sparring matches and seek to learn from each encounter. 
+This choice will lead to further interactions with Hiro, where you'll uncover more about his 
+character and skills, and perhaps even lead to a lethal rivalry""")
 
-chapter2a_option_b = print("""
-    You believe that there must be a way to resolve your differences peacefully. You approach Hiro and propose a truce, suggesting that you both put aside your rivalry for the greater good of the academy. Hiro, surprisingly, agrees to your proposal, and the two of you begin to work together on missions and training exercises. 
-    This choice opens up a different path where you and Hiro become unlikely allies, facing challenges together.""")
-
-chapter2b_option_a = print("""
-    OPTION A
-    You decide to study the forbidden scrolls in secret, curious about the knowledge they hold.""")
-
-chapter2b_option_b = print("""
+def chapter2a_option_b():
+    print("""
     OPTION B
-    Feeling a sense of responsibility, you inform the instructor about the scrolls' discovery, demonstrating your dedication to following the academy's rules.""")
+You believe that there must be a way to resolve your differences peacefully. 
+You approach Hiro and propose a truce, suggesting that you both put aside your rivalry for 
+the greater good of the academy. Hiro, surprisingly, agrees to your proposal, and the two of 
+you begin to work together on missions and training exercises. 
+This choice opens up a different path where you and Hiro become unlikely allies, 
+facing challenges together.""")
+
+def chapter2b_option_a():
+    print("""
+    OPTION A
+You decide to study the forbidden scrolls in secret, curious about the knowledge they hold.""")
+
+def chapter2b_option_b():
+    print("""
+    OPTION B
+Feeling a sense of responsibility, you inform the instructor about the scrolls' discovery, 
+demonstrating your dedication to following the academy's rules.""")
--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 #All imports for the app
 import welcome_text
 import os
+import chapter1
+import chapter2
 clear = lambda: os.system('clear') 
 
 answer_a = ["a", "A"]
 answer_b = ["b", "B"]
-
+answer_yes = ["y", "Y", "YES", "yes"]
+answer_no = ["n", "N", "NO", "no"]
 
 welcome_text.welcome_logo()
 welcome_text.introduction()
@@ -14,4 +17,32 @@ name = input('ENTER YOUR NINJA NAME >> ')
 clear()
 
 welcome_text.start_message(name)
-
+while True:
+    first_answer = input('Would you like to start your adventure? ENTER [yes / no]: >>')
+    if first_answer in answer_yes:
+        clear()
+        chapter1.chapter1()
+        chapter1.chapter1_option_a()
+        chapter1.chapter1_option_b()
+        chapter1_answer = input('What would you like to do? ENTER [a / b] >>')
+        if chapter1_answer in answer_a:
+            clear()
+            chapter2.chapter2a()
+            chapter2.chapter2a_option_a()
+            chapter2.chapter2a_option_b()
+        elif chapter1_answer in answer_b:
+            clear()
+            chapter2.chapter2b()
+            chapter2.chapter2b_option_a()
+            chapter2.chapter2b_option_b()
+        else:
+            print("Oops! It seems like you've entered an invalid choice. Please enter 'a' or 'b'.")
+        
+        break
+    elif first_answer in answer_no:
+        print("Alright, maybe next time. Farewell, cosmic ninja!")
+        break
+    else:
+        print("""Oops! It seems like you've entered a cosmic hiccup in the space-time continuum. 
+        Give it another shot and remember, even cosmic ninjas make mistakes now and then! 
+        Please enter 'yes' or 'no'.""")
