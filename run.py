1c1837b8b9fe1d9e5d1ad0b68da926a0f159fdfd
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-24 14:49:03 +0200

    add while loop for chapter 1

--- run.py
+++ run.py
@@ -28,51 +28,61 @@ while True:
         chapter1.chapter1_option_b()
         print(new_line)
         chapter1_answer = input('What would you like to do? ENTER [a / b] >>')
-        if chapter1_answer in answer_a:
-            clear()
-            chapter2.chapter2a()
-            chapter2.chapter2a_option_a()
-            chapter2.chapter2a_option_b()
-            print(new_line)
-            chapter2a_answer = input('What would you like to do? ENTER [a / b] >>')
-            if chapter2a_answer in answer_a:
+        # loop for the choice in chapter 1
+        while True:
+            if chapter1_answer in answer_a:
                 clear()
-                chapter3.chapter3aa()
-                chapter3.chapter3aa_option_a()
-                chapter3.chapter3aa_option_b()
+                chapter2.chapter2a()
+                chapter2.chapter2a_option_a()
+                chapter2.chapter2a_option_b()
                 print(new_line)
-                chapter3aa_answer = input('What would you like to do? ENTER [a / b] >>')
-            elif chapter2a_answer in answer_b:
+                chapter2a_answer = input('What would you like to do? ENTER [a / b] >>')
+                if chapter2a_answer in answer_a:
+                    clear()
+                    chapter3.chapter3aa()
+                    chapter3.chapter3aa_option_a()
+                    chapter3.chapter3aa_option_b()
+                    print(new_line)
+                    chapter3aa_answer = input('What would you like to do? ENTER [a / b] >>')
+                elif chapter2a_answer in answer_b:
+                    clear()
+                    chapter3.chapter3ab()
+                    chapter3.chapter3ab_option_a()
+                    chapter3.chapter3ab_option_b()
+                    print(new_line)
+                    chapter3ab_answer = input('What would you like to do? ENTER [a / b] >>')
+                else:
+                    print("""Epic cosmic blunder! It happens to the best of us. 
+                    Go ahead, rewrite your cosmic destiny with a keystroke!""")
+                break # exit the loop for chapter1 
+            elif chapter1_answer in answer_b:
                 clear()
-                chapter3.chapter3ab()
-                chapter3.chapter3ab_option_a()
-                chapter3.chapter3ab_option_b()
+                chapter2.chapter2b()
+                chapter2.chapter2b_option_a()
+                chapter2.chapter2b_option_b()
                 print(new_line)
-                chapter3ab_answer = input('What would you like to do? ENTER [a / b] >>')
-        elif chapter1_answer in answer_b:
-            clear()
-            chapter2.chapter2b()
-            chapter2.chapter2b_option_a()
-            chapter2.chapter2b_option_b()
-            print(new_line)
-            chapter2b_answer = input('What would you like to do? ENTER [a / b] >>')
-            if chapter2b_answer in answer_a:
-                clear()
-                chapter3.chapter3ba()
-                chapter3.chapter3ba_option_a()
-                chapter3.chapter3ba_option_b()
-                print(new_line)
-                chapter3ba_answer = input('What would you like to do? ENTER [a / b] >>')
-            elif chapter2b_answer in answer_b:
-                clear()
-                chapter3.chapter3bb()
-                chapter3.chapter3bb_option_a()
-                chapter3.chapter3bb_option_b()
-                print(new_line)
-                chapter3bb_answer = input('What would you like to do? ENTER [a / b] >>')
-        else:
-            print("""Uh-oh, it appears your ninja instincts are momentarily eclipsed by the cosmic chaos. 
-            Take a deep breath, focus your energy, and try again with the precision of a laser ninja-star! Please enter 'a' or 'b'.""")
+                chapter2b_answer = input('What would you like to do? ENTER [a / b] >>')
+                if chapter2b_answer in answer_a:
+                    clear()
+                    chapter3.chapter3ba()
+                    chapter3.chapter3ba_option_a()
+                    chapter3.chapter3ba_option_b()
+                    print(new_line)
+                    chapter3ba_answer = input('What would you like to do? ENTER [a / b] >>')
+                elif chapter2b_answer in answer_b:
+                    clear()
+                    chapter3.chapter3bb()
+                    chapter3.chapter3bb_option_a()
+                    chapter3.chapter3bb_option_b()
+                    print(new_line)
+                    chapter3bb_answer = input('What would you like to do? ENTER [a / b] >>')
+                else:
+                    print("""Hold on a moment, it appears your cosmic ninja keyboard skills need a calibration. 
+                    Try anew and aim for the accuracy of a laser-guided shuriken!""")
+                break # exit the loop for chapter1
+            else:
+                print("""Uh-oh, it appears your ninja instincts are momentarily eclipsed by the cosmic chaos. 
+                Take a deep breath, focus your energy, and try again with the precision of a laser ninja-star! Please enter 'a' or 'b'.""")
         
         break
     elif first_answer in answer_no:
