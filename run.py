fb4c86132f81c91cbd030839de9ea15bcd21f195
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-25 14:55:25 +0200

    split welcome logo and welcome text with a while loop, and give the choice to read story behind app

--- run.py
+++ run.py
@@ -18,6 +18,17 @@ new_line = '\n'
 
 # game start
 welcome_text.welcome_logo()
+welcome_answer = input('Would you like to read more about why I made this app ENTER [a], would you instead become the lethal ninja living deep inside you ENTER [b] >> ')
+while True: #welcome page with logo
+    if welcome_answer in answer_a:
+
+        break # exit welcome page/logo loop
+    elif welcome_answer in answer_b:
+        clear()
+        break # exit welcome page/logo loop
+    else:
+        print("Uh-oh, it appears your ninja instincts are momentarily eclipsed by the cosmic chaos. Take a deep breath, focus your energy, and try again with the precision of a laser ninja-star! Please enter 'a' or 'b'.")
+        welcome_answer = input('Would you like to read more about why I made this app ENTER [a], would you instead become the lethal ninja living deep inside you ENTER [b] >> ')
 welcome_text.introduction()
 name = input('ENTER YOUR NINJA NAME >> ')
 clear()
