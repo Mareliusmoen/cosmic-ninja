ce8a7fdb25c6965157397dd3119c63eb86b7c0db
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-28 12:17:49 +0200

    fix chapter1.py errors found when PEP8 test in CIs Python Linter

--- chapter1.py
+++ chapter1.py
@@ -1,4 +1,4 @@
-def chapter1(): 
+def chapter1():
     """
     Print chapter 1 to terminal.
 
@@ -20,18 +20,20 @@ _/__|___/___/_______________/__|____\_____________/__/__________|\____
 
     Chapter 1: The Galactic Academy
 
-In a galaxy far beyond our own, where stars burned brighter than the sharpest 
-blades and cosmic winds whispered secrets of ancient arts, you find yourself at 
-the threshold of the Galactic Ninja Academy. The journey to becoming a cosmic 
-ninja begins here. The academy stands as a testament to the celestial marvels 
-of the cosmos. Vast, spiraling corridors stretch into the distance, adorned 
-with holographic constellations. The students, a diverse group from across the 
-galaxy, move with grace and purpose. Some practice their laser ninja-weapon 
-skills, while others engage in intense combat simulations. The academy is alive 
+In a galaxy far beyond our own, where stars burned brighter than the sharpest
+blades and cosmic winds whispered secrets of ancient arts, you find yourself at
+the threshold of the Galactic Ninja Academy. The journey to becoming a cosmic
+ninja begins here. The academy stands as a testament to the celestial marvels
+of the cosmos. Vast, spiraling corridors stretch into the distance, adorned
+with holographic constellations. The students, a diverse group from across the
+galaxy, move with grace and purpose. Some practice their laser ninja-weapon
+skills, while others engage in intense combat simulations. The academy is alive
 with the energy of ambition.
-Master Shinobi, the headmaster of the academy, is a figure of great respect and 
-authority. His eyes have witnessed countless cosmic warriors rise and fall. 
-His wisdom is legendary, and his stern but fair guidance is sought after by all.""")
+Master Shinobi, the headmaster of the academy, is a figure of great respect and
+authority. His eyes have witnessed countless cosmic warriors rise and fall. His
+wisdom is legendary, and his stern but fair guidance is sought after by all.
+""")
+
 
 def chapter1_option_a():
     """
@@ -47,11 +49,12 @@ def chapter1_option_a():
     """
     print("""
 OPTION A
-You approach Master Shinobi with determination, eager to introduce yourself and 
-seek his guidance on your journey to becoming a cosmic ninja.   """)
+You approach Master Shinobi with determination, eager to introduce yourself and
+seek his guidance on your journey to becoming a cosmic ninja.""")
+
 
 def chapter1_option_b():
     print("""
 OPTION B
-You decide to blend into the crowd and observe the various training sessions, 
+You decide to blend into the crowd and observe the various training sessions,
 contemplating how you'll navigate this new world.""")
