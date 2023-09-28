65a08bbc734667e22c15d589d5dd42b5fda5045c
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-28 12:27:00 +0200

    fix chapter3.py errors found when PEP8 test in CIs Python Linter

--- chapter3.py
+++ chapter3.py
@@ -18,18 +18,19 @@ def chapter3aa():
            .            .    .     . .   . .  + . . .        ! /
             *    *   .  *  . +..           .    . .        - O -
           .           .    .  +   . .    *   .  **  .    . / |
-                  . .  +  . + .  .  .  .. +  .    . +..     
+                  . .  +  . + .  .  .  .. +  .    . +..
     Chapter 3: Embrace the Rivalry
 
-Your decision to embrace the rivalry with Hiro has led to a series of intense 
-sparring matches and training sessions. Each encounter pushes both of you to 
-your limits, and you learn valuable lessons from one another. The rivalry 
-between you two has evolved into a mutual respect for each other's skills. 
-One day, while sparring in the zero-gravity training room, you both notice a 
-hidden compartment in the wall. Inside, you discover a set of encrypted data 
-chips containing classified information about a powerful cosmic artifact known 
-as the "Starshard." The Starshard is said to possess unimaginable energy and 
+Your decision to embrace the rivalry with Hiro has led to a series of intense
+sparring matches and training sessions. Each encounter pushes both of you to
+your limits, and you learn valuable lessons from one another. The rivalry
+between you two has evolved into a mutual respect for each other's skills.
+One day, while sparring in the zero-gravity training room, you both notice a
+hidden compartment in the wall. Inside, you discover a set of encrypted data
+chips containing classified information about a powerful cosmic artifact known
+as the "Starshard." The Starshard is said to possess unimaginable energy and
 is rumored to have the ability to reshape the galaxy itself.""")
+
 
 def chapter3ab():
     """
@@ -54,14 +55,15 @@ def chapter3ab():
                   . .  +  . + .  .  .  .. +  .    . +..
     Chapter 3: Peaceful Resolution
 
-Your decision to seek a peaceful resolution with Hiro has led to a surprising 
-turn of events. Hiro, once your fierce rival, has become your ally in the 
-pursuit of cosmic ninja excellence. Together, you both put aside your 
-differences and focus on collaborative training and missions. One day, while 
-exploring the academy's library, you stumble upon an ancient tome that hints at 
+Your decision to seek a peaceful resolution with Hiro has led to a surprising
+turn of events. Hiro, once your fierce rival, has become your ally in the
+pursuit of cosmic ninja excellence. Together, you both put aside your
+differences and focus on collaborative training and missions. One day, while
+exploring the academy's library, you stumble upon an ancient tome that hints at
 the location of a hidden cosmic artifact known as the "Starshard".
-The Starshard is said to possess unimaginable energy and is rumored to have the 
+The Starshard is said to possess unimaginable energy and is rumored to have the
 ability to reshape the galaxy itself.""")
+
 
 def chapter3ba():
     """
@@ -83,18 +85,19 @@ def chapter3ba():
            .            .    .     . .   . .  + . . .        ! /
             *    *   .  *  . +..           .    . .        - O -
           .           .    .  +   . .    *   .  **  .    . / |
-                  . .  +  . + .  .  .  .. +  .    . +..   
+                  . .  +  . + .  .  .  .. +  .    . +..
     Chapter 3: Secrets of the Forbidden Scrolls
 
-With a sense of intrigue and defiance, you decide to delve into the forbidden 
-scrolls, despite the risk. Late at night, you sneak into the archives and 
-carefully unlock the holographic shield guarding the ancient knowledge. 
-The scrolls reveal cosmic ninja techniques and insights that were deemed too 
-dangerous to be known. As you immerse yourself in the forbidden knowledge, you 
-begin to understand the immense power these techniques can grant. However, you 
-also become aware of the potential dangers they pose if used irresponsibly. 
-It becomes evident that mastering these techniques will require great 
+With a sense of intrigue and defiance, you decide to delve into the forbidden
+scrolls, despite the risk. Late at night, you sneak into the archives and
+carefully unlock the holographic shield guarding the ancient knowledge.
+The scrolls reveal cosmic ninja techniques and insights that were deemed too
+dangerous to be known. As you immerse yourself in the forbidden knowledge, you
+begin to understand the immense power these techniques can grant. However, you
+also become aware of the potential dangers they pose if used irresponsibly.
+It becomes evident that mastering these techniques will require great
 discipline and responsibility.""")
+
 
 def chapter3bb():
     """
@@ -116,18 +119,19 @@ def chapter3bb():
            .            .    .     . .   . .  + . . .        ! /
             *    *   .  *  . +..           .    . .        - O -
           .           .    .  +   . .    *   .  **  .    . / |
-                  . .  +  . + .  .  .  .. +  .    . +..  
+                  . .  +  . + .  .  .  .. +  .    . +..
     Chapter 3: The Academy's Decision
 
-Your sense of responsibility leads you to make a difficult but principled 
-choice. You promptly inform the academy instructor about the discovery of the 
-forbidden scrolls, demonstrating your dedication to the academy's rules and 
-values. The instructor commends your honesty and escorts you to the academy's 
+Your sense of responsibility leads you to make a difficult but principled
+choice. You promptly inform the academy instructor about the discovery of the
+forbidden scrolls, demonstrating your dedication to the academy's rules and
+values. The instructor commends your honesty and escorts you to the academy's
 council chamber, where a meeting is convened to address this grave matter.
-Inside the council chamber, the academy's most esteemed mentors and leaders 
-gather to discuss the scrolls' discovery. Their discussions are filled with 
-weighty deliberations about the potential consequences of possessing such 
+Inside the council chamber, the academy's most esteemed mentors and leaders
+gather to discuss the scrolls' discovery. Their discussions are filled with
+weighty deliberations about the potential consequences of possessing such
 ancient and powerful cosmic ninja techniques.""")
+
 
 def chapter3aa_option_a():
     """
@@ -143,9 +147,10 @@ def chapter3aa_option_a():
     """
     print("""
     OPTION A
-You and Hiro decide to investigate the Starshard further on your own, believing 
-that your combined skills as cosmic ninjas can unlock its secrets and 
+You and Hiro decide to investigate the Starshard further on your own, believing
+that your combined skills as cosmic ninjas can unlock its secrets and
 ensure its safekeeping.""")
+
 
 def chapter3aa_option_b():
     """
@@ -161,9 +166,10 @@ def chapter3aa_option_b():
     """
     print("""
     OPTION B
-Feeling the weight of this discovery, you decide to report the existence of the 
-Starshard and the data chips to Master Shinobi and the academy authorities, 
+Feeling the weight of this discovery, you decide to report the existence of the
+Starshard and the data chips to Master Shinobi and the academy authorities,
 concerned about the potential dangers it may pose.""")
+
 
 def chapter3ab_option_a():
     """
@@ -179,9 +185,10 @@ def chapter3ab_option_a():
     """
     print("""
     OPTION A
-You and Hiro decide to embark on a quest to find the Starshard, believing that 
-your newfound partnership will give you an advantage in uncovering its secrets 
+You and Hiro decide to embark on a quest to find the Starshard, believing that
+your newfound partnership will give you an advantage in uncovering its secrets
 and protecting it from potential threats.""")
+
 
 def chapter3ab_option_b():
     """
@@ -197,10 +204,11 @@ def chapter3ab_option_b():
     """
     print("""
     OPTION B
-Considering the gravity of this discovery, you choose to report the existence 
-of the Starshard and the information in the ancient tome to Master Shinobi and 
-the academy authorities, hoping for their guidance in handling such a powerful 
+Considering the gravity of this discovery, you choose to report the existence
+of the Starshard and the information in the ancient tome to Master Shinobi and
+the academy authorities, hoping for their guidance in handling such a powerful
 artifact.""")
+
 
 def chapter3ba_option_a():
     """
@@ -216,9 +224,10 @@ def chapter3ba_option_a():
     """
     print("""
     OPTION A
-You continue to study the forbidden scrolls in secret, determined to harness 
-their power and become a cosmic ninja of unparalleled skill, even if it means 
+You continue to study the forbidden scrolls in secret, determined to harness
+their power and become a cosmic ninja of unparalleled skill, even if it means
 breaking academy rules.""")
+
 
 def chapter3ba_option_b():
     """
@@ -235,8 +244,9 @@ def chapter3ba_option_b():
     print("""
     OPTION B
 You decide to halt your exploration of the forbidden scrolls and return them to
-their hiding place, realizing the risks involved and choosing to prioritize 
+their hiding place, realizing the risks involved and choosing to prioritize
 the safety of the academy.""")
+
 
 def chapter3bb_option_a():
     """
@@ -252,8 +262,9 @@ def chapter3bb_option_a():
     """
     print("""
     Option A
-You choose to support the academy's decision to destroy the forbidden scrolls, 
+You choose to support the academy's decision to destroy the forbidden scrolls,
 believing that their power could lead to chaos if misused.""")
+
 
 def chapter3bb_option_b():
     """
@@ -269,5 +280,6 @@ def chapter3bb_option_b():
     """
     print("""
     Option B
-You advocate for preserving the forbidden scrolls for further study, arguing 
-that there may be ways to harness their power responsibly for the greater good.""")
+You advocate for preserving the forbidden scrolls for further study, arguing
+that there may be ways to harness their power responsibly for the greater good.
+""")
