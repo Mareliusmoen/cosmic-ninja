f065924b1cb4bf918ecf4f34913306cf497efb77
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-28 13:34:00 +0200

    fix wrong_input.py errors found when PEP8 test in CIs Python Linter

--- wrong_input.py
+++ wrong_input.py
@@ -1,29 +1,100 @@
 def wrng_input1():
+    """
+    Print wrong input message nr 1 to terminal.
+
+    This function prints the multiline text string representing wrong input
+    message nr 1.
+
+    Note:
+        For this function to work in run.py file, I've imported this file
+        to make the files functions available to call in run.py.
+
+    """
     print("""
-Whoa, it's like your ninja skills got entangled in a cosmic knot! Try once more,
-and let's untangle this cosmic mystery together!""")
+Whoa, it's like your ninja skills got entangled in a cosmic knot! Try once
+more, and let's untangle this cosmic mystery together!""")
+
 
 def wrng_input2():
+    """
+    Print wrong input message nr 2 to terminal.
+
+    This function prints the multiline text string representing wrong input
+    message nr 2.
+
+    Note:
+        For this function to work in run.py file, I've imported this file
+        to make the files functions available to call in run.py.
+
+    """
     print("""
-Hold on a moment, it appears your cosmic ninja keyboard skills need a 
+Hold on a moment, it appears your cosmic ninja keyboard skills need a
 calibration. Try anew and aim for the accuracy of a laser-guided shuriken!""")
 
+
 def wrng_input3():
+    """
+    Print wrong input message nr 3 to terminal.
+
+    This function prints the multiline text string representing wrong input
+    message nr 3.
+
+    Note:
+        For this function to work in run.py file, I've imported this file
+        to make the files functions available to call in run.py.
+
+    """
     print("""
-Epic cosmic blunder! It happens to the best of us. Go ahead, rewrite your 
+Epic cosmic blunder! It happens to the best of us. Go ahead, rewrite your
 cosmic destiny with a keystroke!""")
 
+
 def wrng_input4():
+    """
+    Print wrong input message nr 4 to terminal.
+
+    This function prints the multiline text string representing wrong input
+    message nr 4.
+
+    Note:
+        For this function to work in run.py file, I've imported this file
+        to make the files functions available to call in run.py.
+
+    """
     print("""
 Looks like you've entered the intergalactic labyrinth of choices. Don't worry,
-even cosmic ninjas sometimes take detours. 
+even cosmic ninjas sometimes take detours.
 Try your cosmic navigation skills again!""")
 
+
 def wrng_input5():
+    """
+    Print wrong input message nr 5 to terminal.
+
+    This function prints the multiline text string representing wrong input
+    message nr 5.
+
+    Note:
+        For this function to work in run.py file, I've imported this file
+        to make the files functions available to call in run.py.
+
+    """
     print("""
-Whoopsie-daisy! Your cosmic command got lost in the astral shuffle. Give it 
+Whoopsie-daisy! Your cosmic command got lost in the astral shuffle. Give it
 another whirl, and may the cosmic winds be in your favor!""")
 
+
 def wrng_name():
-    print("That's not a kick-ass ninja name! I know you" 
-    " can do better than that, try again...")
+    """
+    Print wrong input message for 'player name input' to terminal.
+
+    This function prints the multiline text string representing wrong input
+    for 'player name input'.
+
+    Note:
+        For this function to work in run.py file, I've imported this file
+        to make the files functions available to call in run.py.
+
+    """
+    print("That's not a kick-ass ninja name! I know you"
+          " can do better than that, try again...")
