9cf847b043829ec11ffa227cc93fe41d0c35e660
Mareliusmoen <mareliusmoen@gmail.com>
2023-09-28 09:39:08 +0200

    add return command in front of input to make function work as intended

--- queries.py
+++ queries.py
@@ -1,5 +1,5 @@
 def start_y_or_n():
-    input("""Would you like to start your adventure? ENTER [yes / no]: >>  """)
+    return input("""Would you like to start your adventure? ENTER [yes / no]: >>  """)
 
 def a_or_b():
-    input('What would you like to do? ENTER [a / b] >>  ')
+    return input('What would you like to do? ENTER [a / b] >>  ')
